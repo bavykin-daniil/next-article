888b68cc85abf6ae242178265f5861dd58913934
Daniil <bavykin.daniil.inc@gmail.com>
2022-04-30 19:24:13 +0300

    Downloading project into git

---
+++ src/components/Application/Application.jsx
@@ -0,0 +1,12 @@
+//general
+import React from "react";
+//components
+
+//styles
+import styles from "./Application.module.scss";
+
+const Application = () => {
+  return <div className={styles.application}>Hello world</div>;
+};
+
+export default Application;
